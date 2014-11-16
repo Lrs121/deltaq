f9547bf6c18e326a66de21deffd118c5d566d056
scorpion <scorpion.jze@gmail.com>
2014-11-16 05:16:19 -0600

    Added VcDiffTests Added failing VcDiffCreate test

---
+++ deltaq-tests/VcDiffTests.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace deltaq_tests
+{
+    [TestFixture]
+    public class VcDiffTests
+    {
+        [Test]
+        public void VcDiffCreate()
+        {
+            var buf = new byte[0x1000];
+
+            byte[] outBytes;
+            using (var outStream = new MemoryStream())
+            {
+                deltaq.VcDiff.Create(buf, buf, outStream);
+            }
+        }
+    }
+}
